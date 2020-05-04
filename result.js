91c45a55dea11817d023619dab7351b9c07fee0d
Darren Crack <masterdanish2512@gmail.com>
2020-05-04 14:42:23 +0800

    Create result.js

---
+++ result.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $.fn.showResult = function () {
+        showUserPoints();
+        showUserStats();
+        showScoreboard();
+        showQuestionReviews();
+    }
+
+    $.fn.showUserPoints = function () {
+        let scoreArea = $('#user-score').val();
+    }
+
+    $.fn.showUserStats = function () {
+        let scoreArea = $('#user-score').val();
+    }
+
+    $.fn.showScoreboard = function () {
+        let scoreArea = $('#user-score').val();
+    }
+
+    $.fn.showQuestionReviews = function () {
+        let qReview = $('#question-review').val();
+    }
+})
