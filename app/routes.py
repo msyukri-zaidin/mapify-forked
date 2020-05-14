24ad0a839a7c6396242887271d921d4322261381
mazza <msyukri.zaidin@gmail.com>
2020-05-14 21:40:52 +0800

    Removed print statements, added route for getting new question data and added code for filtering current questions from questionPool table

--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,47 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app import db
-from app.models import Question
+from app.models import Question, CurrentQuestion
 from app.forms import QuestionForm
 import sys
+import json
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = QuestionForm()
 
     print(form.errors)
-    if form.is_submitted():
-        print("submitted")
-    else:
-        print("UNSUBMITTED")
-    if form.validate():
-        print("valid")
-    else:
-        print("INVALID")
 
     if form.validate_on_submit():
-        #flash(form.question.data)
-        
-        q = Question(question = form.question.data, answer = form.answer.data, difficulty = form.difficulty.data)
+        q = Question(question = form.question.data, answer = form.answer.data)
         db.session.add(q)
         db.session.commit()
-
         return redirect(url_for('index'))
 
-    return render_template('admin_page.html', questionPool = Question.query.all(), form = form)
+    
+    questionPool = Question.query.all()
+    currentQuestion = CurrentQuestion.query.all()
+    filterList = [1,2,3,4,5]
+    for cQuestion in currentQuestion:
+        for pQuestion in questionPool:
+            if cQuestion.id == pQuestion.id:
+                filterList.remove(cQuestion.id)
+    questionPool = Question.query.filter(Question.id.in_(filterList)).all()
+
+    return render_template('admin_page.html', questionPool = questionPool, form = form, currentQuestion = currentQuestion)
 
 @app.route('/base')
 def base():
     return render_template('base.html')
+
+@app.route('/postmethod', methods = ['POST'])
+def get_question_data():
+    convertedQuestionNumber = int(request.form['questionNumber'])
+    questionData = {convertedQuestionNumber : (request.form['questionID'], request.form['questionContent'], request.form['questionAnswer'])}
+    CurrentQuestion.query.get(convertedQuestionNumber).id = request.form['questionID']
+    CurrentQuestion.query.get(convertedQuestionNumber).question = request.form['questionContent']
+    CurrentQuestion.query.get(convertedQuestionNumber).answer = request.form['questionAnswer']
+    db.session.commit()
+    return redirect(url_for('index'))
