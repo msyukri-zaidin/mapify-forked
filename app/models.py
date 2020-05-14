cb8e304d2e6e768ee7afb5339b195dac6d6709b5
mazza <msyukri.zaidin@gmail.com>
2020-05-14 21:36:13 +0800

    Removed difficulty column and added CurrentQuestion table

--- app/models.py
+++ app/models.py
@@ -2,11 +2,22 @@ from app import db
 
 class Question(db.Model):
 
-    #Initialise questioon statement
+    #Initialise question statement
     id = db.Column(db.Integer, primary_key = True)
     question = db.Column(db.String(128))
     answer = db.Column(db.String(128))
-    difficulty = db.Column(db.String(64))
+
+    #Print current question
+    def __repr__(self):
+        return self.question
+
+class CurrentQuestion(db.Model):
+
+    #Initialise questioon statement
+    number = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer)
+    question = db.Column(db.String(128))
+    answer = db.Column(db.String(128))
 
     #Print current question
     def __repr__(self):
