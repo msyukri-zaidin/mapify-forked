90b5b08ad6b2717513f9f130fe9978f131e97847
mazza <msyukri.zaidin@gmail.com>
2020-05-16 11:19:45 +0800

    Added question type field

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, RadioField
+from wtforms import StringField, SubmitField, RadioField, TextAreaField
 from wtforms.validators import DataRequired
 from wtforms.validators import InputRequired
 
 class QuestionForm(FlaskForm):
-    question = StringField("Question")
+    question = TextAreaField("Question")
+    questionType = RadioField('Type', choices=[('multiple-choice', 'Multiple-Choice'), ('short-answer', 'Short-Answer')])
     answer = StringField("Answer")
 #, validators = [DataRequired()]
-    submit = SubmitField("Submit"
+    submit = SubmitField("Submit")
