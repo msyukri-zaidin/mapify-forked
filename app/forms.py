713a49355af80c216317fb5ebe81bae52c704655
mazza <msyukri.zaidin@gmail.com>
2020-05-17 20:15:50 +0800

    Added Question set form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, RadioField, TextAreaField
+from wtforms import StringField, SubmitField, RadioField, TextAreaField, IntegerField
 from wtforms.validators import DataRequired
 from wtforms.validators import InputRequired
 
@@ -8,4 +8,9 @@ class QuestionForm(FlaskForm):
     questionType = RadioField('Type', choices=[('multiple-choice', 'Multiple-Choice'), ('short-answer', 'Short-Answer')])
     answer = StringField("Answer")
 #, validators = [DataRequired()]
+    submit = SubmitField("Submit")
+
+class QuestionsetForm(FlaskForm):
+    name = StringField("Name")
+    number_of_questions = IntegerField("Number of Questions")
     submit = SubmitField("Submit")
