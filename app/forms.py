24bd90545f9d897b81c7496376f3a8ba41ce37a3
mazza <msyukri.zaidin@gmail.com>
2020-05-18 15:26:24 +0800

    Add option fields for multiple-choice

--- app/forms.py
+++ app/forms.py
@@ -6,7 +6,12 @@ from wtforms.validators import InputRequired
 class QuestionForm(FlaskForm):
     question = TextAreaField("Question")
     questionType = RadioField('Type', choices=[('multiple-choice', 'Multiple-Choice'), ('short-answer', 'Short-Answer')])
-    answer = StringField("Answer")
+    short_answer = StringField("Answer")
+    multiple_choice_answer = StringField("Answer")
+    option_value_1 = StringField("Option")
+    option_value_2 = StringField("Option")
+    option_value_3 = StringField("Option")
+    option_value_4 = StringField("Option")
 #, validators = [DataRequired()]
     submit = SubmitField("Submit")
 
