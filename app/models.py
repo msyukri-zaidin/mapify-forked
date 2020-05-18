a46722b43b61ca9cdc53bc93f121afea946746b6
mazza <msyukri.zaidin@gmail.com>
2020-05-18 15:27:06 +0800

    Add Option table schema

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class Question(db.Model):
     question_type = db.Column(db.String(128))
     answer = db.Column(db.String(128))
     child = db.relationship("CurrentQuestion", backref="parent")
+    option_child = db.relationship("Option", backref="parent")
 
     #Print current question
     def __repr__(self):
@@ -15,7 +16,7 @@ class Question(db.Model):
 
 class CurrentQuestion(db.Model):
     __tablename__ = 'current_question'
-    #Initialise questioon statement
+    #Initialise current question statement
     
     id = db.Column(db.Integer, primary_key = True)
     question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
@@ -28,20 +29,20 @@ class CurrentQuestion(db.Model):
 
 class QuestionSet(db.Model):
     __tablename__ = 'questionset'
+    #Initialise question set statement
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(64))
     number_of_questions = db.Column(db.Integer)
     child = db.relationship("CurrentQuestion")
+
     def __repr__(self):
         return self.name
-        
-'''
-class Option(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    #question_id
-    option_value = db.Column(db.String(64))
-    is_answer = db.Column(db.Boolean, default=False)
 
-        def __repr__(self):
+class Option(db.Model):
+    #Initialise option statement
+    id = db.Column(db.Integer, primary_key = True)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+    option_value = db.Column(db.String(64))
+
+    def __repr__(self):
         return self.option_value
-'''
