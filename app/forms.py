5bd4f3fa1b0886eaf6da943f1e8854aa090999f6
mazza <msyukri.zaidin@gmail.com>
2020-05-21 21:32:45 +0800

    Add RegistrationForm

--- app/forms.py
+++ app/forms.py
@@ -32,3 +32,8 @@ class LoginForm(FlaskForm):
     username = StringField("Username", validators = [DataRequired()])
     password = PasswordField("Password", validators = [DataRequired()])
     submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators = [DataRequired()])
+    password = PasswordField("Password", validators = [DataRequired()])
+    submit = SubmitField('Sign Up')
